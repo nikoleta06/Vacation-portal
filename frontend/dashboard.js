6fe65abd4d0cf5a12696095af1a46f17187bae42
Nikoleta Akrida <nikol.akrida@gmail.com>
2025-10-14 18:23:47 +0300

    I changed the appearance of the welcome message to start with the name and not the email or something else.

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const userEmail = localStorage.getItem("userEmail");
-    const userEmailDisplay = document.getElementById("userEmailDisplay");
-    const logoutBtn = document.getElementById("logoutBtn");
+    const welcomeText = document.getElementById("welcomeText");
+    
 
     if (userEmail) {
-        userEmailDisplay.textContent = "Welcome, ${userEmail}";
+          
+        // We remove the part before the @ so it appears as a name//
+        const userName = userEmail.split("@")[0];
+
+        // We capitalize the first letter (e.g. maria -> Maria)//
+        const formattedName = userName.charAt(0).toUpperCase() + userName.slice(1);
+
+        welcomeText.textContent = `Welcome, ${formattedName}!`;
     } else{
         window.location.href= "index.html"; // If there is no user, go back to login
     }
 
+    const logoutBtn = document.getElementById("logoutBtn");
     logoutBtn.addEventListener("click", () => {
         localStorage.removeItem("userEmail");
         window.location.href = "index.html";
